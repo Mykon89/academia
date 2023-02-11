fb6771ac67d349a23f51d0fc5786889e68de77fd
Mykon <helvis89@gmail.com>
2023-02-11 10:05:25 -0300

    tela login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form action="" className="login-form">
+            <span className="login-form-title">
+              <img src="#" alt="logo" />
+            </span>
+
+            <span className="login-form-title">Login</span>
+
+            <div className="wrap-input">
+              <input
+                className={email !== '' ? 'has-val input' : 'input'}
+                type="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Email" />
+            </div>
+
+            <div className="wrap-input">
+              <input
+                className={password !== '' ? 'has-val input' : 'input'}
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Password" />
+            </div>
+
+            <div className="container-login-form-btn">
+              <button className="login-form-btn">Login</button>
+            </div>
+
+            <div className="text-center">
+              <span className="txt1">Não possui conta?</span>
+              <a href="#href" id="ahref" className="txt2">
+                Criar conta.
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
